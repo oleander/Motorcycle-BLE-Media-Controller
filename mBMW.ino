d1e3f065ae9bb2517a4a5f36a95af912e9f6dfa0
Linus Oleander <linus.oleander@clark.de>
2023-04-24 15:16:34 +0200

    Use serial for debug messages

--- mBMW.ino
+++ mBMW.ino
@@ -1,46 +1,57 @@
 #include <BleKeyboard.h>
 #include <Arduino.h>
 #include <OneButton.h>
+#include <NimBLEDevice.h>
 
-BleKeyboard bleKeyboard("uBMW");
+BleKeyboard bleKeyboard("uBMW", "701", 100);
 
 #define BUTTON_PIN 33
 
 OneButton btn = OneButton(
-  BUTTON_PIN,  // Input pin for the button
-  true,        // Button is active LOW
-  true         // Enable internal pull-up resistor
+  BUTTON_PIN,
+  true,
+  true
 );
 
 void setup() {
+  Serial.begin(9600); // Initialize serial communication
+  Serial.println("Device started");
+
+  btn.setClickTicks(400);
+  btn.setDebounceTicks(100);
+
   bleKeyboard.begin();
 
-  // Single click
   btn.attachClick([]() {
     if (bleKeyboard.isConnected()) {
       bleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
     }
+    Serial.println("Button single click");
   });
 
-  // Double click
   btn.attachDoubleClick([]() {
     if (bleKeyboard.isConnected()) {
       bleKeyboard.write(KEY_MEDIA_NEXT_TRACK);
     }
+    Serial.println("Button double click");
   });
 
-  // Tripple click
   btn.attachMultiClick([]() {
     if (bleKeyboard.isConnected()) {
       bleKeyboard.write(KEY_MEDIA_PREVIOUS_TRACK);
     }
+    Serial.println("Button multi click");
   });
 
-  // Hold
   btn.attachLongPressStart([]() {
     if (bleKeyboard.isConnected()) {
       bleKeyboard.write(KEY_MEDIA_VOLUME_UP);
     }
+    Serial.println("Button long press start");
+  });
+
+  btn.attachLongPressStop([]() {
+    Serial.println("Button long press stop");
   });
 }
 
